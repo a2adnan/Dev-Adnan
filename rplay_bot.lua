635794d9378918758d9d9f0dc01728ac69d329b7
a2adnan <adnan15saleh@gmail.com>
2026-05-22 05:43:53 +0300

    Update developer contact link in rplay_bot.lua

--- rplay_bot.lua
+++ rplay_bot.lua
@@ -6,7 +6,7 @@ if ( msg.text ) then
 
   if ( msg.to.type == "user" ) then
 
-     return  "للتحدث مع المطور اضغط على المعرف التالي \n @O_q1j \n  👾 "
+     return  "للتحدث مع المطور اضغط على المعرف التالي \n https://karboai.com/user/ims2t \n  👾 "
      
   end 
    
